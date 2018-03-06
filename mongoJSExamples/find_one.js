b35cc0e2f39efdc8aede876c82944339615e09a9
des9409 <des9409@rit.edu>
2018-03-05 20:58:50 -0500

    Fixed find_one.js

--- mongoJSExamples/find_one.js
+++ mongoJSExamples/find_one.js
@@ -4,7 +4,7 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("RSSFeed");
-  dbo.collection("RSSData").find({}, { _id: 0, title: 1 }).toArray(function(err, result) {
+  dbo.collection("RSSData").find({}).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
     db.close();
