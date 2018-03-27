45d58f9e60838131b8261ea76f7bbdc0243eaa0d
aphedges <aphedges1@gmail.com>
2018-03-26 21:03:10 -0400

    Changed URL for adding form

--- server/main.js
+++ server/main.js
@@ -32,7 +32,7 @@ app.use('/', express.static(path.join(__dirname, '..', 'client')));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-app.post('/', feedHandler);
+app.post('/add', feedHandler);
 app.post('/delete', deleter);
 app.get('/feed.rss', (req, res) => {
     res.end(rssxml);
