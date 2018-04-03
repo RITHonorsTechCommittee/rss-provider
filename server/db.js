1d13e7da43b6f344aaaf7dc70120c56723e1ede0
aphedges <aphedges1@gmail.com>
2018-04-02 20:51:34 -0400

    Improved DB name duplication

--- server/db.js
+++ server/db.js
@@ -1,13 +1,15 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
+var db_name = "RssFeed";
+var collection_name = "RssData";
 
 // Insert the item, return true on success, false otherwise
 exports.insert = function (item) {
 	MongoClient.connect(url, function(err, db) {
 		if (err) throw err;
-		var dbo = db.db("RSSFeed"); // Opens database
+		var dbo = db.db(db_name); // Opens database
 		var myobj = { title: item.title, link: item.url, description: item.description, author: item.author, pubDate: item.date, expirationDate: item.expirationDate }; // Defines new object
-		dbo.collection("RSSData").insertOne(myobj, function(err, res) { // Inserts the given object
+		dbo.collection(collection_name).insertOne(myobj, function(err, res) { // Inserts the given object
 			if (err) throw err;
 			console.log("1 document inserted");
 			db.close();
@@ -21,8 +23,8 @@ exports.select_all = function () {
 
 	MongoClient.connect(url, function(err, db) {
 		if (err) throw err; // Ensures it is connected 
-		var dbo = db.db("RSSFeed"); // Opens database
-		dbo.collection("RSSData").find({}).toArray(function(err, result) { // Finds all items in collection, excludes id field, and turns to array
+		var dbo = db.db(db_name); // Opens database
+		dbo.collection(collection_name).find({}).toArray(function(err, result) { // Finds all items in collection, excludes id field, and turns to array
 			if (err) throw err;
 			console.log(result); // Prints results
 			final_result = result;
@@ -37,9 +39,9 @@ exports.select_all = function () {
 exports.delete_item = function (id) {
 	MongoClient.connect(url, function(err, db) {
 		if (err) throw err;
-		var dbo = db.db("RSSFeed"); // Opens database
+		var dbo = db.db(db_name); // Opens database
 		var deleteQuerry = {'_id': id} // Deletes any articles with the id
-		dbo.collection("RSSData").deleteOne(deleteQuerry, function(err, res) { // Executes querry
+		dbo.collection(collection_name).deleteOne(deleteQuerry, function(err, res) { // Executes querry
 			if (err) throw err;
 			console.log(res.result.n + " document(s) deleted");
 			db.close();
