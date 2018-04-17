9ac45ab3ea7cb1727e95e351344e1f147e099025
Randy Carlson <randycarlson486@gmail.com>
2018-04-16 21:02:40 -0400

    Fixed frontend deletion

--- client/script.js
+++ client/script.js
@@ -6,8 +6,12 @@ function onLoad() {
     document.getElementById('date').value = `${newDate.getFullYear()}-${('00' + (newDate.getMonth() + 1)).slice(-2)}-${('00' + newDate.getDate()).slice(-2)}`;
 }
 
-function deleteItem(element, guid) {
-    fetch('/delete/' + guid, {
+async function deleteItem(id) {
+    var res = await fetch('/delete/' + id, {
         method: 'POST',
     });
+
+    if(res.ok) {
+        document.getElementById(id).remove();
+    }
 }
